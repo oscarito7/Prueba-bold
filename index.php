633c21176bfac23022c9caf82dfee51f76a65e56
oscarito7 <32312985+oscarito7@users.noreply.github.com>
2020-07-31 07:50:06 -0600

    cambio final de entrega

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
-/*include_once 'config/user.php';
-include_once 'config/user_session.php';
-if(isset($_SESSION['user'])){
-    echo "session";
-}else if(isset($_POST['username']) && isset($_POST['password'])){
-    $userForm = $_POST['username'];
-    $passForm = $_POST['password'];
-    if($user->userExists($userForm, $passForm)){
-            //$userSession->sercurrentUser($userForm);
-            $user->setUser();
-    }else {
-        $errorLogin = "nombre de usuario y/o pass incorrecto";
-        include_once "views/login.php";
-    }
-}else{
-    include_once 'views/login.php';
-}*/
+
 define('HOMEDIR', __DIR__);
 
 include 'views/header.php';
